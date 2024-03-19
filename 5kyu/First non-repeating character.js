355caeedebcb1f701bfe2550dd3baf263393c52a
Andrew Plutto <pluttan@ya.ru>
2024-03-19 17:09:55 +0300

    fc

---
+++ 5kyu/First non-repeating character.js
@@ -0,0 +1,10 @@
+function firstNonRepeatingLetter(s) {
+  s.map(
+    
+  );
+}
+const Test = require("@codewars/test-compat");
+
+Test.assertEquals(firstNonRepeatingLetter("a"), "a");
+Test.assertEquals(firstNonRepeatingLetter("stress"), "t");
+Test.assertEquals(firstNonRepeatingLetter("moonmen"), "e");
